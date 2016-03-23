7f81cf563f2c50d75ebddd10a9bae0a1379a7c73
Andrew Seba <sebaturtle@gmail.com>
2016-03-22 19:12:25 -0500

    Initial Commit

---
+++ InfiniRunner/Assets/Scripts/ObstacleController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleController : MonoBehaviour {
+
+    public GameObject obstaclePrefab;
+    float speed = -3.0f;
+    GameObject playerObj;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        Invoke("SpawnObstacles", 3f);
+	}
+	
+	void SpawnObstacle()
+    {
+        //spawn an obstacle at x10, y -4 to 0
+        Vector3 spawnPos = new Vector3(10, Random.Range(-4, 0), 0);
+        GameObject tempObstacle = Instantiate(obstaclePrefab, spawnPos, Quaternion.Euler(Vector3.zero))as GameObject;
+        tempObstacle.name = "Obstacle";
+
+        if(tempObstacle.GetComponent<Rigidbody2D>() == false)
+        {
+            tempObstacle.AddComponent<Rigidbody2D>();
+        }
+        tempObstacle.GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            Time.timeScale = 0;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ InfiniRunner/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MoveCharacter();
+	}
+
+    void MoveCharacter()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetBool("jump", true);
+            anim.SetBool("run", false);
+        }
+
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetBool("jump", false);
+            anim.SetBool("run", true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetBool("slide", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            anim.SetBool("slide", false);
+        }
+
+    }
+}
